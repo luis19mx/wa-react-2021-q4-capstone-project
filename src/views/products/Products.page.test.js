fefa619e1c78f6f75c11544edd3f1afd32e31810
Luis Lasso <hola@luislasso.com>
2021-11-17 09:19:12 -0600

    Test pagination on product page

--- src/views/products/Products.page.test.js
+++ src/views/products/Products.page.test.js
@@ -12,19 +12,41 @@ describe('/products', () => {
       preloadedState: productsStore,
     });
 
-    const { waitForNextUpdate } = renderHook(() => useFetchProductList(), {
+    const { waitFor } = renderHook(() => useFetchProductList(), {
       wrapper: Wrapper,
     });
 
-    await waitForNextUpdate();
+    await waitFor(() => {
+      const title = screen.getByText(/Our Products/);
+      const prevPageLink = screen.queryAllByRole('link', { name: '<' });
+      const nextPageLink = screen.getByRole('link', { name: '›' });
 
-    const title = screen.getByText(/Our Products/);
-    const prevPageLink = screen.queryAllByRole('link', { name: '<' });
-    const nextPageLink = screen.getByRole('link', { name: '›' });
+      expect(title).toBeInTheDocument();
+      expect(prevPageLink).toHaveLength(0);
+      expect(nextPageLink).toBeInTheDocument();
+      expect(store.getState().products.products).toHaveLength(12);
+    });
+  });
 
-    expect(title).toBeInTheDocument();
-    expect(prevPageLink).toHaveLength(0);
-    expect(nextPageLink).toBeInTheDocument();
-    expect(store.getState().products.products).toHaveLength(12);
+  it('should update history.location.state on click to next link', async () => {
+    const { Wrapper, history } = render(<App />, {
+      route: '/products',
+      preloadedState: productsStore,
+    });
+
+    const { waitFor } = renderHook(() => useFetchProductList(), {
+      wrapper: Wrapper,
+    });
+
+    await waitFor(() => {
+      expect(history.location.state).toBeUndefined();
+
+      const nextPageLink = screen.getByRole('link', { name: '›' });
+      fireEvent.click(nextPageLink);
+
+      const paginationLink = history.location.state.paginationLink;
+      expect(paginationLink).toBeTruthy();
+      expect(paginationLink).toContain('page=2');
+    });
   });
 });
